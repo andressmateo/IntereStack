97389a61933f754684199c595f2406c62069d5bf
andressmateo <andressmateo@andressmateo-HOME.(none)>
2015-03-07 16:01:48 -0500

    Primera versión de la búsqueda con datos quemados

--- www/js/index.js
+++ www/js/index.js
@@ -37,13 +37,35 @@ app.initialize();
 var interestack = angular.module('ui.bootstrap.interestack', ['ui.bootstrap']);
 
 interestack.controller('interestackCtrl', function($scope, $http) {
+	
+	$scope.groups =  ["Calculo 1 - Felix", "Semillero de programación", "Aprendiendo Haskell", 
+	                  "Mecánica de fluidos", "Simulación y modelación", "Cálculo 3", "Monitorías Juan Pablo Álvarez",
+	                  "Observadores de aves", "SERES", "Becados ANDI", "Estudiantes becados", "Ingeniería de Sistemas 2013-1",
+	                  "¡Speak english with us!", "Cálculo 2", "Cálculo 3 - Espitia", "Pre-cálculo"];
+	
 	$scope.template = "main_search.html";
-
-
 	$scope.search = function () {
+		$scope.group_question = document.getElementById("search").value;;
 		$scope.template = "results.html";
 	};
 
 
+});
+
+interestack.filter('search_for_groups', function(){
+
+	return function(arr, group_question){
+		if(!group_question){
+			return arr;
+		}
+		var result = [];
+		group_question = group_question.toLowerCase();
+		angular.forEach(arr, function(item){
+			if(item.toLowerCase().indexOf(group_question) !== -1){
+				result.push(item);
+			}
+		});
+		return result;
+	};
 });
 
