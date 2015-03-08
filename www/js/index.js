2dae81781b8e48c8ec254aa2457f81be4d96f9e8
andressmateo <andressmateo@andressmateo-HOME.(none)>
2015-03-08 18:08:32 -0500

    sin cors pero con otro error

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,39 @@
 var interestack = angular.module('ui.bootstrap.interestack', ['ui.bootstrap']);
 
-interestack.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}
-]);
-
 interestack.controller('interestackCtrl', function($scope, $http) {
 	
-	$scope.groups =  [];
+	/*$scope.groups = ["Calculo 1 - Felix", "Semillero de programación", "Aprendiendo Haskell",
+	                 "Mecánica de fluidos", "Simulación y modelación", "Cálculo 3", "Monitorías Juan Pablo Álvarez",
+	                 "Observadores de aves", "SERES", "Becados ANDI", "Estudiantes becados",
+	                 "Ingeniería de Sistemas 2013-1",
+	                 "¡Speak english with us!", "Cálculo 2", "Cálculo 3 - Espitia", "Pre-cálculo"];
+	*/
 	
-	
-	//$http.get('http://interestack.herokuapp.com/get_gropus', {
-	$http.get('http://127.0.0.1:5000/get_gropus', {
+	$scope.groups = [];
+	/*$http.jsonp('http://interestack.herokuapp.com/get_gropus?callback=JSON_CALLBACK', {
+	//$http.jsonp('http://127.0.0.1:5000/get_gropus?callback=JSON_CALLBACK', {
 	      params: {
 	      }
 	    }).then(function(response){
 	    	response.data.names.map(function(item){
 	    		$scope.groups.push(item.name);
 	        });
-	    });
+	    });*/
+	
+	//$http.jsonp('http://127.0.0.1:5000/get_gropus?callback=JSON_CALLBACK')
+	//url = "http://api.openbeerdatabase.com/v1/beers.json?callback=JSON_CALLBACK";
+	
+	
+	url = "http://interestack.herokuapp.com/get_gropus?callback=JSON_CALLBACK";
+	$http.jsonp(url).success(function(data){
+        /*console.log(data.groups);
+        response.data.groups.map(function(item){
+    		$scope.groups.push(item);
+        });*/
+    }).error(function(data, status, headers, config) {
+        $scope.error = true;
+        alert("error");
+    });
 	
 	
 	$scope.template = "main_search.html";
